ceb638a170b1673a10892fe2ff51296d75b0747e
cgviviana <128948188+cgviviana@users.noreply.github.com>
2025-01-29 17:00:04 -0500

    Update ardoraBooklet.js

--- js/ardoraBooklet.js
+++ js/ardoraBooklet.js
@@ -1,25 +1,70 @@
 $(document).ready(function(){
-   $.fn.booklet.defaults = {
-      name:"",
-      width:1600,height:900,speed:1000,direction:"LTR",
-      startingPage:1,easing:"easeInOutQuad",easeIn:"easeInQuad",easeOut:"easeOutQuad",
-      closedFrontTitle:"Beginning",closedFrontChapter:"Beginning of Book",closedBackTitle:"End",closedBackChapter:"End of Book",
-      covers:true,
-      closed:true,
-      autoCenter:true,
-      pagePadding:10,pageBorder:0,
-	    pageNumbers:true,
-	    manual:false,
-      hovers:true,hoverWidth:50,hoverSpeed:500,hoverThreshold:0.25,overlays:true,tabs:false,tabWidth:60,tabHeight:20,
-      nextControlText:"Next",previousControlText:"Previous",
-      nextControlTitle:"Next",previousControlTitle:"Previous",
-      arrows:false,
-      arrowsHide:false,
-      cursor:"pointer",hash:false,hashTitleText:" - Page ",
-keyboard:true,
-      next:null, prev:null, auto:false, delay:5000, pause:null, play:null, menu:null,pageSelector:false,chapterSelector:false,
-shadows:true,shadowTopFwdWidth:166,shadowTopBackWidth:166,shadowBtmWidth:50,before:function(){},after:function(){}
-      }
- $("#mybook").booklet();$(".b-prev").attr("tabindex","0");$(".b-next").attr("tabindex","0");
+   $("#mybook").booklet({
+      width: "90vw",  // Se adapta al ancho de la pantalla
+      height: "85vh", // Se adapta a la altura de la pantalla
+      speed: 1000,
+      direction: "LTR",
+      startingPage: 1,
+      easing: "easeInOutQuad",
+      easeIn: "easeInQuad",
+      easeOut: "easeOutQuad",
+      closedFrontTitle: "Beginning",
+      closedFrontChapter: "Beginning of Book",
+      closedBackTitle: "End",
+      closedBackChapter: "End of Book",
+      covers: true,
+      closed: true,
+      autoCenter: true,
+      pagePadding: 10,
+      pageBorder: 0,
+      pageNumbers: true,
+      manual: false,
+      hovers: true,
+      hoverWidth: 50,
+      hoverSpeed: 500,
+      hoverThreshold: 0.25,
+      overlays: true,
+      tabs: false,
+      tabWidth: 60,
+      tabHeight: 20,
+      nextControlText: "Next",
+      previousControlText: "Previous",
+      nextControlTitle: "Next",
+      previousControlTitle: "Previous",
+      arrows: true, // Activar flechas de navegación para mejorar la interacción
+      arrowsHide: false,
+      cursor: "pointer",
+      hash: false,
+      hashTitleText: " - Page ",
+      keyboard: true,
+      next: null,
+      prev: null,
+      auto: false,
+      delay: 5000,
+      pause: null,
+      play: null,
+      menu: null,
+      pageSelector: true, // Activar selector de páginas
+      chapterSelector: false,
+      shadows: true,
+      shadowTopFwdWidth: 166,
+      shadowTopBackWidth: 166,
+      shadowBtmWidth: 50,
+      before: function(){},
+      after: function(){}
+   });
+
+   $(".b-prev").attr("tabindex","0");
+   $(".b-next").attr("tabindex","0");
 });
-function playIt(sound){var sounds=document.getElementsByTagName("audio");for(i=0; i<sounds.length; i++) sounds[i].pause();var file=document.getElementById(sound);file.load();file.play();}
+
+// Función para reproducir sonidos correctamente
+function playIt(sound){
+   var sounds = document.getElementsByTagName("audio");
+   for(var i = 0; i < sounds.length; i++) {
+      sounds[i].pause();
+   }
+   var file = document.getElementById(sound);
+   file.load();
+   file.play();
+}
